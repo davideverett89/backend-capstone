80b128074ac2525a3172c8b13f1b77480ff5eb0b
David Everett <davideverett1989@gmail.com>
2020-09-10 10:47:38 -0500

    Inital setup complete.

--- virtumarketapp/urls.py
+++ virtumarketapp/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
