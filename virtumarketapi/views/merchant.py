720180c7cdbc333911cf431d0dc43c267b393c15
David Everett <davideverett1989@gmail.com>
2020-09-13 03:03:33 -0500

    Defined custom method to filter a single merchant by current user.

--- virtumarketapi/views/merchant.py
+++ virtumarketapi/views/merchant.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
+from rest_framework.decorators import action
 from virtumarketapi.models import Merchant, Good
 from django.contrib.auth.models import User
 from .good import GoodSerializer
@@ -21,7 +22,6 @@ from .good import GoodSerializer
 #             "date_joined",
 #             "email"
 #         )
-
 class MerchantSerializer(serializers.HyperlinkedModelSerializer):
 
     # user = UserSerializer()
@@ -65,23 +65,23 @@ class Merchants(ViewSet):
 
     def list(self, request):
 
-        if request.user.id:
-            print('You should get one merchant')
-            merchant = Merchant.objects.filter(user_id=request.user.id)
-            serializer = MerchantSerializer(
-            merchant,
+        merchants = Merchant.objects.all()
+
+        serializer = MerchantSerializer(
+            merchants,
             many=True,
             context={"request": request}
-            )
-            return Response(serializer.data)
-        else:
-            print('You should get all merchants')
-            merchants = Merchant.objects.all()
+        )
+        return Response(serializer.data)
 
-            serializer = MerchantSerializer(
-                merchants,
-                many=True,
-                context={"request": request}
-            )
-            return Response(serializer.data)
-    
+    @action(detail=False)
+    def current_user(self, request):
+        user = self.request.user
+
+        merchant = Merchant.objects.get(user_id=user.id)
+        
+        serializer = MerchantSerializer(
+        merchant,
+        context={"request": request}
+        )
+        return Response(serializer.data)
