73477f9e375241a389f76a4bce8eb6c5b4474611
David Everett <davideverett1989@gmail.com>
2020-09-14 10:56:21 -0500

    Added delete method on goods viewset.

--- virtumarketapi/views/good.py
+++ virtumarketapi/views/good.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
+from rest_framework import status
 from virtumarketapi.models import Good, Merchant, GoodType, UnitSize
 
 class GoodSerializer(serializers.HyperlinkedModelSerializer):
@@ -78,3 +79,16 @@ class Goods(ViewSet):
             context={"request": request}
         )
         return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+
+        try:
+            good = Good.objects.get(pk=pk)
+            good.delete()
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Good.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
