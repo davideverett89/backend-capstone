c70a075d22c508a1218ea74851b36438ef8d64ff
David Everett <davideverett1989@gmail.com>
2020-09-15 10:19:58 -0500

    Added more fields to market, consumer, and merchant models, adjusted market fixtures accordingly.

--- virtumarketapi/models/consumer.py
+++ virtumarketapi/models/consumer.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import User
 
 class Consumer(models.Model):
 
-    image = models.CharField(max_length=500)
+    bio = models.CharField(max_length=300, default="")
+    profile_image = models.CharField(max_length=500, default="")
     phone_number = models.CharField(max_length=50)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     
     class Meta:
         verbose_name = ("consumer")
         verbose_name_plural = ("consumers")
-        ordering = (F('user.date_joined').asc(nulls_last=True),)
 
     def __str__(self):
         return "{} {}".format(self.first_name, self.last_name)
--- virtumarketapi/models/merchant.py
+++ virtumarketapi/models/merchant.py
@@ -7,7 +7,9 @@ class Merchant(models.Model):
     """Class that defines the Merchants model."""
 
     company_name = models.CharField(max_length=50)
-    image = models.CharField(max_length=500)
+    bio = models.CharField(max_length=300, default="")
+    profile_image = models.CharField(max_length=500, default="")
+    booth_image = models.CharField(max_length=500, default="")
     phone_number = models.CharField(max_length=50)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="merchant")
     market = models.ForeignKey(Market, on_delete=models.CASCADE, related_name="merchants")
