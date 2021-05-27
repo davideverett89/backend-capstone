20a37a8cc5f2c38beee567bc3e1bae1cd0b6fadc
David Everett <davideverett1989@gmail.com>
2021-05-27 18:52:07 -0500

    I don't even know anymore.

--- virtumarketapp/settings.py
+++ virtumarketapp/settings.py
@@ -109,6 +109,10 @@ WSGI_APPLICATION = 'virtumarketapp.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('NAME'),
